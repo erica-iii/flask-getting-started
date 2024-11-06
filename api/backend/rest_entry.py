06306a738ccc1b0a14203928732da93ca7859128
Mark Fontenot <m.fontenot@northeastern.edu>
2024-11-06 07:57:52 -0500

    Basic structure of a simple Flask app started

---
+++ api/backend/rest_entry.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import make_response
+from backend.db_connection import db
+import os
+from dotenv import load_dotenv
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load environment variables
+    # This function reads all the values from inside
+    # the .env file (in the parent folder) so they
+    # are available in this file.  See the MySQL setup 
+    # commands below to see how they're being used.
+    load_dotenv()
+
+    # secret key that will be used for securely signing the session 
+    # cookie and can be used for any other security related needs by 
+    # extensions or your application
+    # app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    # # these are for the DB object to be able to connect to MySQL. 
+    # app.config['MYSQL_DATABASE_USER'] = 'root'
+    app.config['MYSQL_DATABASE_USER'] = os.getenv('DB_USER').strip()
+    app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_ROOT_PASSWORD').strip()
+    app.config['MYSQL_DATABASE_HOST'] = os.getenv('DB_HOST').strip()
+    app.config['MYSQL_DATABASE_PORT'] = int(os.getenv('DB_PORT').strip())
+    app.config['MYSQL_DATABASE_DB'] = os.getenv('DB_NAME').strip()  # Change this to your DB name
+
+    # Initialize the database object with the settings above. 
+    app.logger.info('current_app(): starting the database connection')
+    db.init_app(app)
+
+    @app.route("/", methods=['GET'])
+    def sayHello():
+        response = make_response("Hello from your First Flask Api App")
+        response.status_code = 200
+        return response
+
+    # Don't forget to return the app object (back to the
+    # backend_app function in this case)
+    return app
+
